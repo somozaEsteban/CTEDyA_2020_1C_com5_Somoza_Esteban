2a0790d45542810a6e4dc5a13acfc15923041abb
esteban somoza <63671432+somozaEsteban@users.noreply.github.com>
2020-04-24 20:04:34 -0300

    Add files via upload

---
+++ trabajo1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ArrozSama
+ * Date: 24/04/2020
+ * Time: 19:47
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace trabajo1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
